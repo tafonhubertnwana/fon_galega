068465ed3948bdade465e60fb0897b250711eab9
Tafon Herbert <tafonsoftwarespecialist@gmail.com>
2025-08-31 18:29:37 +0100

    changing the loge

--- components/fonHistory.js
+++ components/fonHistory.js
@@ -3,7 +3,7 @@
 
 import { motion } from "framer-motion";
 import Image from "next/image";
-import fonImg from "@/public/fon/log-2.jpg"; // Replace with your image path
+import fonImg from "@/public/fon/HistoryImg.jpg"; // Replace with your image path
 
 export default function FonHistory() {
   return (
